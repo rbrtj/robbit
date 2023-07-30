918e3e830ac5c908c0bfb734c0fae444fa0845bd
rbrtj <robertjaszczurek@icloud.com>
2023-07-30 21:29:01 +0200

    styles import fix + prisma dependency install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
-import '@styles/globals.css';
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
 import type { Metadata } from 'next';
-
+import { Inter } from 'next/font/google';
 export const metadata: Metadata = {
   title: 'Robbit',
   description: 'Reddit clone',
 };
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -12,8 +15,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-500 antialiased">
+        {/* <Navbar /> */}
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
