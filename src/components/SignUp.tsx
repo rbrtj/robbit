3da962bb848b316bc0d99610ccca9046fed43c36
rbrtj <robertjaszczurek@icloud.com>
2023-08-03 23:49:35 +0200

    SignUp component - added todo

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import UserAuthForm from './UserAuthForm';
 
+// TODO: Fix close button on modal.
 const SignUp = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
